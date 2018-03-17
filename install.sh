7f2a6c21df63853ea516012ec21bf8662cc2b8d2
ranjujohn@gmail.com <ranjujohn@gmail.com>
2018-03-17 15:54:45 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ip=$1
+wget -q https://s3.eu-central-1.amazonaws.com/leverton-dev-test/hello.jar
+mkdir -p /app
+cp hello.jar /app
+
+tee /etc/systemd/system/webapp.service << EOF
+[Unit]
+Description=Web App
+
+[Service]
+ExecStart=/usr/bin/java -jar /app/hello.jar
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable webapp
+sudo systemctl start webapp
